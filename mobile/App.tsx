e0ee819b8737ed2c007ab1af35af77a425ee7cb1
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2023-01-25 01:25:02 -0300

    Finalizando tela inicial do app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,8 @@
-import { Loading } from './src/components/Loading'
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
+import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, useFonts } from '@expo-google-fonts/inter';
+import { StatusBar } from 'react-native';
+import { Loading } from './src/components/Loading';
+import './src/lib/dayjs';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -14,22 +16,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app! AAAHHH</Text>
-      <StatusBar barStyle="light-content" backgroundColor="tranparent" translucent />
-    </View>
+    <>
+      <Home />
+      <StatusBar barStyle="light-content" translucent/>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#F3FFF3',
-    fontFamily: 'Inter_600SemiBold'
-  }
-});
